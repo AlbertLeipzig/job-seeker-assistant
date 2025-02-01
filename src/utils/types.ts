698925dd847c0bcd15b1c8b4080ff38788408dd3
AlbertLeipzig <albertclemvill@gmail.com>
2025-02-01 13:43:02 +0100

    finished

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,21 +1,51 @@
 import { SetStateAction, Dispatch } from "react";
 
+export interface IApplication {
+  id: string;
+  title: string;
+  link: string;
+  techStack: string[];
+  contactPerson: string;
+  company: string;
+  date: Date;
+  feedback: IPosting[];
+}
+
+export interface ICompany {
+  name: string;
+  site: string;
+}
+
 export type TError = null | string | Error;
 
 export type TLoading = boolean;
 
+export interface INote {
+  date: Date;
+  text: string[];
+}
+
+export interface IPerson {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export interface IPosting {
+  id: string;
+  company: string;
+  date: Date;
+  contactPerson: string;
+  feedbackNotes: INote[];
+}
+
+export interface ITech {
+  name: string;
+  categories: string[];
+  amount: number;
+}
+
 export type TTheme = "light" | "dark";
-
-export interface IState{
-  state: boolean | string;
-  setState: Dispatch<SetStateAction<unknown>>;
-}
-
-
-
-export interface IStateProps {
-  stateProps: IState;
-}
 
 export interface IAppContext {
   theme: TTheme;
@@ -24,8 +54,14 @@ export interface IAppContext {
   setError: Dispatch<SetStateAction<TError>>;
   loading: TLoading;
   setLoading: Dispatch<SetStateAction<TLoading>>;
+  applications: null | IApplication[];
+  setApplications: Dispatch<SetStateAction<null | IApplication[]>>;
+  postings: null | IPosting[];
+  setPostings: Dispatch<SetStateAction<null | IPosting[]>>;
+  techs: null | ITech[];
+  setTechs: Dispatch<SetStateAction<null | ITech[]>>;
 }
 
-export interface IAppChildren {
+export interface IAppContextChildren {
   children: React.ReactNode;
 }
