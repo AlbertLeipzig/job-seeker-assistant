c4b2529b1d99fe5a458c951b452c8ce7a43d864b
AlbertLeipzig <albertclemvill@gmail.com>
2025-02-02 19:12:30 +0100

    finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style/index.scss";
-import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./utils/AppRouter";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={AppRouter} />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<AppRouter />);
